3bfe00d0f20fcd7df0ddbe1f0b06f1da449ec930
MrZend <vlad04102002@gmail.com>
2020-12-16 18:30:14 +0200

    Доработан MenuController, Catalog, Company, CRM

--- DeliverySystem/UI_Controllers/DialogController.cs
+++ DeliverySystem/UI_Controllers/DialogController.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeliverySystem.DataModels;
 
 namespace DeliverySystem.UI_Controllers
 {
     class DialogController
     {
         // Управление диалогами
+        public void DialogWithCatalogManager()
+        {
+
+        }
+
+        public void DialogWithCompanyManager()
+        {
+
+        }
+
+        public void DialogWithCRMManager()
+        {
+
+        }
 
     }
 }
